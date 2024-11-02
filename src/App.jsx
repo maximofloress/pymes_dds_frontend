d8ec6c41ed3ede7438c4aae45c4b86796387175b
maximofloress <95429@sistemas.frc.utn.edu.ar>
2024-11-02 12:56:40 -0300

    proyecto vite creado + install de estilos bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+     <div className="mt-4 p-5 rounded" style={{backgroundColor:"lightgray"}} >
+        <h1>Pymes 2024</h1>
+        <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+        <p>
+          Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y 
+          múltiples capas en Javascript.
+        </p>
+        <p>
+          Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
+          Javascript y React.
+        </p>
+        <button className="btn btn-lg btn-primary">
+          <i className="fa fa-search"> </i>
+          Ver Articulos Familias
+        </button>
+      </div>
+  );
+}
+export default App;
