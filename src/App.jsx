c31a0be51b15631fc3277303be3a5b554e5b954b
maximofloress <95429@sistemas.frc.utn.edu.ar>
2024-11-02 13:53:40 -0300

    Componente Menu + impementacion React Router + Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-  import "./App.css";
-  import {ArticulosFamilias} from './components/ArticulosFamilias';
-  function App() {
-    return (
-      <>
-        <div className="divBody">
-            <ArticulosFamilias/>
-        </div>
-      </>
-    );
-  }
-  export default App;
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {Menu} from "./components/Menu";
+import { Footer } from "./components/Footer";
+import { Inicio } from "./components/Inicio";
+import { ArticulosFamilias } from "./components/ArticulosFamilias";
+function App() {
+  return (
+    <>
+        <BrowserRouter>
+          <Menu />
+          <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route
+                path="/articulosfamilias"
+                element={<ArticulosFamilias />}
+              />
+              <Route path="*" element={<Navigate to="/inicio" replace />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  );
+}
+export default App;
